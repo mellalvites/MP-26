ecc931d974a84edd5f1f5ca851adb4d764103585
mellalvites <mell.almiya21@gmail.com>
2026-02-04 19:55:17 +0100

    04-02

---
+++ JS/campo-pruebas/main.js
@@ -0,0 +1,23 @@
+console.log('Hola desde el otro lado')
+
+const arrayCoches = ['cocheAzul', 'cocheVerde',4]
+
+
+console.log(arrayCoches[0])
+
+
+const lado1 = 16;
+const lado2 = 13;
+const lado3 = 23;
+
+volumen = lado1*lado2*lado3
+
+console.log(volumen)
+
+const arrayHobbies = ['Leer Cómics', 'Jugar videojuegos','Pádel'];
+
+console.log(arrayHobbies[1])
+
+arrayHobbies.push("Comer")
+
+console.log(arrayHobbies)
